a12730d0afbcd7bf934aae386bee3eea47d2318f
khj3753 <khj3753@gmail.com>
2025-04-22 04:19:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 99.81 ms, Memory: 27.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    enumerate
+    for index, value in enumerate(arr):
+        if index == 0:
+            answer.append(value)
+        elif value != answer[-1]:
+            answer.append(value)
+            
+    return answer
