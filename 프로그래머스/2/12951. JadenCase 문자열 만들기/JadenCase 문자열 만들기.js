d6c4f0ed3a8e04c1703e31d9cdf2c122676ad1cc
khj3753 <khj3753@gmail.com>
2025-05-27 16:10:40 +0900

    [chore] 아쉬운 점 주석으로 보강(12951. JadenCase 문자열 만들기)

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,7 @@
 function solution(s) {
+    // str[0] => charAt, slice => subslice를 사용하면 한줄로 표현 가능
+    // s.split(" ").map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(" ");
     return s.split(" ").map((str) => {
-        console.log(str)
         if (str === "") {
             return ""
         } else {
