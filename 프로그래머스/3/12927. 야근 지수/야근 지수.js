028f1a0a65a06860f7627b31d58cf3eafe365d98
khj3753 <khj3753@gmail.com>
2025-05-30 15:54:50 +0900

    [level 3] Title: 야근 지수, Time: 1.13 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -8,11 +8,6 @@ function solution(n, works) {
         let pivot = 0
         let index = works.length - 1
         for (let i in works) {
-            // 최소 값 기준으로 구했을 때 n을 넘기는지 확인
-            // 2 15 22 55 55 = 149 / 99
-            // 2 * 5 = 10 => 139 > 99
-            // 15 * 4 = 60 => 87 < 99 => 12
-            // >>  2 15 15 15 15
             let overtime = works[i] * (works.length - i)
             if ((total_work - overtime) > worktime) {
                 total_work -= works[i];
@@ -24,10 +19,6 @@ function solution(n, works) {
                 break;
             }
         }
-        console.log(index, pivot)
-        console.log(answer)
-        console.log(worktime)
-        
         if (works.length != index) {
             const rest_works_count = works.length - index
             const q = pivot - parseInt(worktime / rest_works_count);
