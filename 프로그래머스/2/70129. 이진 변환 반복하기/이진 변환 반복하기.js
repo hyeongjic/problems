6d6f5dea1a13bf4ac98ef3b2c13a5def67801473
khj3753 <khj3753@gmail.com>
2025-06-04 10:50:48 +0900

    [chore] 다른 코드로 풀이(70129. 이진 변환 반복하기)

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -17,3 +17,21 @@ function solution(s) {
     
     return [executeCount, zeroCount];
 }
+
+/*
+filter 사용
+function solution(s) {
+    let zeroCount = 0;
+    let executeCount = 0;
+    
+    while(s != "1") {
+        executeCount++;
+        s = s.split("").filter(element => {
+            if(element === "0") zeroCount++;
+            return element === "1"
+        }).length.toString(2);
+    }
+    
+    return [executeCount, zeroCount];
+}
+*/
