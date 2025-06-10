aed6d301c6ec0e64806a163eec7c862efcbf2fd6
khj3753 <khj3753@gmail.com>
2025-06-10 14:20:53 +0900

    [chore] 추가 주석 작성(42840. 모의고사)

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -4,7 +4,11 @@ function solution(answers) {
     const mathGiver_2 = [2,1,2,3,2,4,2,5];
     const mathGiver_3 = [3,3,1,1,2,2,4,4,5,5];
     const scores = [0 ,0 ,0];
-    
+
+    // 3명이라 하드 코딩으로 넣긴 했는데 더 많은 사람 수를 대응 가능하게 짜는것도 고려해볼껄 싶다
+    // 그나마 아래 스코어 구하는 방법은 사람이 많아도 대응은 됨
+    // filter를 활용하는 방법도 좋은 것 같다
+    // scores[0] = answers.filter((a,i)=> a === mathGiver_1[i%mathGiver_1.length]).length;
     for (let i in answers) {
         if (answers[i] === mathGiver_1[i%mathGiver_1.length]) scores[0]++;
         if (answers[i] === mathGiver_2[i%mathGiver_2.length]) scores[1]++;
