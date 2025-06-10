f2405a6daa44404bfbf4baa4d757f51c15d58c12
khj3753 <khj3753@gmail.com>
2025-06-10 14:28:33 +0900

    [chore] 추가 풀이 작성(77884. 약수의 개수와 덧셈)

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,5 +1,7 @@
 function solution(left, right) {
     var answer = 0;
+    // 숫자에 루트를 씌웠을때 정수면 그 숫자의 약수는 홀수이다
+    // Number.isInteger(Math.sqrt(number))
     for (let i = left; i <= right; i++) {
         let count = (i === 1 ? 1 : 2);
         for(let j=2; j <= Math.ceil(i/2); j++) {
