75e81afe7552f3c67e86ecbce23abd7190020b57
khj3753 <khj3753@gmail.com>
2025-06-11 14:45:08 +0900

    [chore] 아쉬운 점 주석으로 보강(120956. 옹알이(1))

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -18,3 +18,20 @@ function solution(babbling) {
     }
     return answer;
 }
+
+/*
+문제를 맞추긴 했지만 좋지 않은 방법이라 생각
+문자열을 확인하는건 정규식을 활용해서 풀 수 있도록 노력 필요
+
+function solution(babbling) {
+  var answer = 0;
+  const regex = /^(aya|ye|woo|ma)+$/;
+
+  babbling.forEach(word => {
+    if (regex.test(word)) answer++;  
+  })
+
+  return answer;
+}
+
+*/
